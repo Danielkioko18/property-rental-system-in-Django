4753c944807d834878b141ac8bf91eaa948c108b
dennismwangi-k <denixxdenixx64@gmail.com>
2024-09-09 00:22:28 +0300

    d

--- pystay/settings.py
+++ pystay/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-+39x6i6#7cfqy2nz7@25fq09cr-pyc1i@+8fp17v=sj=kj%r9=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
