c5f3a2279f5b252138e1662fb49f8a18399b4b0b
dennismwangi-k <denixxdenixx64@gmail.com>
2024-10-04 15:52:58 +0300

    sd

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,10 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='auth'),
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', UserRegisterView.as_view(), name='auth'),
+ 
+
+   path('login/', custom_login_view, name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/update/', UserUpdateView.as_view(), name='profile_update'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,13 +3,15 @@ from django.views.generic import CreateView,UpdateView
 from .forms import *
 from .models import *
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 
 class UserRegisterView(CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = 'auth-page.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('auth')
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
@@ -17,4 +19,30 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     form_class = CustomUserUpdateForm
     template_name = 'profile_update.html'
     success_url = reverse_lazy('profile')
+
+
+
+
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.contrib import messages
+
+def custom_login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  
+            return redirect('property')  
+        else:
+            messages.error(request, 'Invalid username or password')  
+
+    return render(request, 'auth-page.html')
 
